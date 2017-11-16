a1b26119eeefa70ee878f6340b2d81e931ff81bc
magnus <magnus@semikomma.is>
2017-11-16 17:25:57 +0000

    moved pairs to own file

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 
 import Pusher from "pusher-js";
+import Pairs from "./src/pairs";
 
 const PUSHER_KEY = 'de504dc5763aeef9ff52';
 
 const pusher = new Pusher(PUSHER_KEY);
 
-const Pairs= {
-  BTC_USD: 'btcusd',
-  BTC_EUR: 'btceur',
-  EUR_USD: 'eurusd',
-  XPR_USD: 'xrpusd',
-  XPR_EUR: 'xrpeur',
-  XPR_BTC: 'xrpbtc',
-  LTC_USD: 'ltcusd',
-  LTC_EUR: 'ltceur',
-  LTC_BTC: 'ltcbtc',
-  ETH_USD: 'ethusd',
-  ETH_EUR: 'etheur',
-  ETH_BTC: 'ethbtc'
-};
+
 
 let monkey = function(pair) {
   const ticker = pair.substring(0, 3).toUpperCase();
---
+++ src/pairs.js
@@ -0,0 +1,16 @@
+const Pairs= {
+  BTC_USD: 'btcusd',
+  BTC_EUR: 'btceur',
+  EUR_USD: 'eurusd',
+  XPR_USD: 'xrpusd',
+  XPR_EUR: 'xrpeur',
+  XPR_BTC: 'xrpbtc',
+  LTC_USD: 'ltcusd',
+  LTC_EUR: 'ltceur',
+  LTC_BTC: 'ltcbtc',
+  ETH_USD: 'ethusd',
+  ETH_EUR: 'etheur',
+  ETH_BTC: 'ethbtc'
+};
+
+module.exports = Pairs;
